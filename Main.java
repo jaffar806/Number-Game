9b4f63264a36273d9a187b4ea52213efabebae8b
jaffar806 <146517898+jaffar806@users.noreply.github.com>
2023-10-15 21:43:17 +0530

    Update Main.java
    i just added some extra information inside last System.out.println

--- Main.java
+++ Main.java
@@ -33,7 +33,7 @@ public class Main {
 		     }
 		  }
 		
-		System.out.println("you have taken "+count+" tries");
+		System.out.println("you have taken "+count+" tries to find the random number");
 	
       	
 
